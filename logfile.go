3560edc7b8eb6426af1a65376910229e072a5335
Armando Couto <coutoarmando@gmail.com>
2020-06-02 12:15:32 -0300

    Refatorando

--- logfile.go
+++ logfile.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"runtime"
 	"time"
 )
 
-func CreateFileDay(message string, env string) {
-	if runtime.GOOS == env {
+func main() {
+	CreateFileDay("Olá", true)
+}
+
+func CreateFileDay(message string, production bool) {
+	if production {
 		f, err := os.OpenFile(fmt.Sprint(time.Now().Format("20060102"), ".log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 		if err != nil {
 			log.Println(err)
